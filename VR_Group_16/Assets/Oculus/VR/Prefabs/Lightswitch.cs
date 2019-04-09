d0cb2eb366ca3753e1bb6d10e2de636b3a5c81a1
Batteryman212 <austinborn212@gmail.com>
2019-04-09 11:50:13 -0500

    Added initial earth bending scene plus Oculus core SDK

---
+++ VR_Group_16/Assets/Oculus/VR/Prefabs/Lightswitch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightswitch : MonoBehaviour {
+	public Light lighting;
+	private int currentColor;
+
+	// Use this for initialization
+	void Start () {
+		lighting = GetComponent<Light>();
+		currentColor = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("tab")){
+			currentColor = (currentColor + 1) % 6;
+			switch(currentColor){
+				case 0: 
+					lighting.color = Color.white;
+					break;
+				case 1: 
+					lighting.color = Color.red;
+					break;
+				case 2: 
+					lighting.color = Color.green;
+					break;
+				case 3: 
+					lighting.color = Color.blue;
+					break;
+				case 4: 
+					lighting.color = Color.cyan;
+					break;
+				case 5: 
+					lighting.color = Color.magenta;
+					break;
+				case 6: 
+					lighting.color = Color.yellow;
+					break;
+			}
+		}
+	}
+}
---
+++ VR_Group_16/Assets/Scripts/EarthWall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(0,-1,2);
+        this.GetComponent<MeshRenderer>().enabled = false;
+	}
+	
+	//Update is called once per frame
+	void Update () {
+        // If both controller triggers activate
+            // Wall position goes to below ground, 3 m in front of average controller direction
+            // Record average current position of controllers
+            // Calculate average vertical displacement from recorded position
+            // If displacement is positive
+                // If displacement is above max amount
+                    // Wall reaches max height
+                // Else
+                    // Wall moves up the amount of displacement
+            // Else
+                // Wall stays at min position
+        // Else
+            // Wall experiences gravity
+    }
+}
+
