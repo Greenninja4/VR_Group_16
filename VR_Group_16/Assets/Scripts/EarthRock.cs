5ccf12819a93f3845c9826fb738e0021f05a25c1
Batteryman212 <austinborn212@gmail.com>
2019-04-19 13:11:30 -0500

    Finished earth bending rock wall, working on rock throw, and updated OVR Player controller prefab

--- VR_Group_16/Assets/Scripts/EarthRock.cs
+++ VR_Group_16/Assets/Scripts/EarthRock.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class EarthRock : MonoBehaviour {
 
+    public bool rock_active;
+    public int hand; // 0 = none, 1 = Left, 2 = Right
+
 	// Use this for initialization
 	void Start () {
 		// Disable
+        rock_active = false;
+        rock_visible = false;
+        this.GetComponent<MeshRenderer>().enabled = false;
+        this.GetComponent<Rigidbody>().isKinematic = false;
+
+        //Default hand to none
+        hand = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// If chosen and not enabled
+
+        // If chosen and not enabled
+        if (rock_active){
+
             // Enable
+            if(!rock_visible){
+                this.GetComponent<MeshRenderer>().enabled = true;
+                this.GetComponent<Rigidbody>().isKinematic = true;
+                rock_visible = true;
+            }
+            
+
+        }
+
+		
+            
             // If active left
                 // Set active hand to left
             // Else
