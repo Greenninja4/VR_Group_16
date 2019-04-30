1f24b17cc81d6b89dc1ca5178b21354e29cf25fb
Chris <chris@DESKTOP-9MKBTE3.localdomain>
2019-04-30 03:14:17 -0500

    Beginning of achievements done

---
+++ VR_Group_16/Assets/Scripts/Menus/Achievements.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Achievements : MonoBehaviour
+{
+    //Bound to a main achievements menu
+    //Amongst the children are the other acievements menus
+    public Transform menu;
+
+    private Dictionary<string, GameObject> submenus = new Dictionary<string, GameObject>();
+
+    //Current things that require submenus
+    private string[] approved_submenus = { "AirMenu", "FireMenu", "WaterMenu", "EarthMenu", "TopMenu" };
+
+    //Text data for each tile on the menus
+    private static Dictionary<string, string> fireMenuText = new Dictionary<string, string>()
+    {
+        {"Time", "Time spent: " },
+        {"Shots", "Fire balls: " },
+        {"Hits", "Hits: " },
+        {"Accuracy", "Accuracy: " },
+        {"Back", "Quit" },
+        {"Title", "Fire Achievements" }
+    };
+    private static Dictionary<string, string> waterMenuText = new Dictionary<string, string>()
+    {
+        {"Time", "Time spent: " },
+        {"Shots", "Water balls: " },
+        {"Hits", "Hits: " },
+        {"Accuracy", "Accuracy: " },
+        {"Back", "Quit" },
+        {"Title", "Water Achievements" }
+
+    };
+    private static Dictionary<string, string> earthMenuText = new Dictionary<string, string>()
+    {
+        {"Time", "Time spent: " },
+        {"Shots", "Rock balls: " },
+        {"Hits", "Hits: " },
+        {"Accuracy", "Accuracy: " },
+        {"Back", "Quit" },
+        {"Title", "Earth Achievements" }
+
+    };
+    private static Dictionary<string, string> airMenuText = new Dictionary<string, string>()
+    {
+       { "Time", "Time spent: " },
+       { "Shots", "Air balls: " },
+       { "Hits", "Hits: " },
+       { "Accuracy", "Accuracy: " },
+       { "Back", "Quit" },
+        {"Title", "Air Achievements" }
+
+    };
+
+    private static Dictionary<string, string> topMenuText = new Dictionary<string, string>()
+    {
+       { "Fire", "Fire" },
+       { "Water", "Water" },
+       { "Earth", "Earth" },
+       { "Air", "Air: " },
+       { "Quit", "Quit" },
+       { "Title", "Achievements" }
+
+    };
+
+    private Dictionary<string, Dictionary<string, string>> menuText = new Dictionary<string, Dictionary<string, string>>()
+    {
+        { "FireMenu", fireMenuText },
+        { "WaterMenu", waterMenuText },
+        { "EarthMenu", earthMenuText },
+        { "AirMenu", airMenuText },
+        { "TopMenu", topMenuText } 
+    };
+
+
+
+    private Dictionary<string, string> currentMenuText;
+    private string[] currentMenuOptions;
+    private GameObject field;
+    private string curr_submenu;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        this.menu = this.transform;
+        this.curr_submenu = "TopMenu";
+        //Get each achievement submenu from main achievement menu
+        //child will be any gameObject attached to the achievement 
+        ///menu window
+        foreach (Transform child in menu)
+        {
+            //Remove self and null from list (to avoid null ptr error
+            if (child == null) continue;
+            else if (child == this.gameObject) continue; //transfer to gameObject required
+
+            //If name matches submenu name, add to list
+            else if (approved_submenus.Contains(child.name))
+            {
+                Debug.Log("Adding " + child.name + " to submenus");
+                this.submenus.Add(child.name, child.gameObject);
+            }
+        }
+
+
+        //For each submenu we have a name for, populate it's text field
+        //with stored data
+        foreach (string key in submenus.Keys)
+        {
+            submenus[key].SetActive(false);
+
+            //currentMenuText is dictionary where each key is a submenu 
+            //and each value is the text that should populate that field
+            this.currentMenuText = menuText[key];
+            foreach (string menuOption in currentMenuText.Keys)
+            {
+                //submenu->menu option
+                this.field = this.submenus[key].transform.Find(menuOption).gameObject;
+                Debug.Log(this.field);
+                //menu option -> canvas -> text
+                this.field.transform.Find("Canvas").transform.Find("Text").GetComponent<Text>().text = currentMenuText[menuOption];
+            }
+        }
+
+        clickHandle(submenus["FireMenu"]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void clickHandle(GameObject selectedItem)
+    {
+        GameObject hitMenu;
+        this.submenus.TryGetValue(selectedItem.name, out hitMenu);
+
+        if (hitMenu != null)
+        {
+            Debug.Log(hitMenu.name);
+            this.submenus[this.curr_submenu].gameObject.SetActive(false);
+            hitMenu.SetActive(true);
+            this.curr_submenu = selectedItem.name;
+        }
+
+        if (selectedItem.name == "Back")
+        {
+            this.submenus[curr_submenu].SetActive(false);
+            this.menu.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ VR_Group_16/Assets/Scripts/Menus/AchievementsMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementsMenu : MonoBehaviour {
+
+    // Status variable & menu object
+    public GameObject menu;
+
+    // Ray cast controller
+    private float triggerThreshold = 0.5f;
+    private float triggerThresholdLower = 0.1f;
+    private bool clicked = false;
+    public OVRInput.Controller controller;
+    public Transform laserEnd, laserBegin;
+    private LineRenderer laserLine;
+    public Vector3 hitpoint;
+    public GameObject selectedItem;
+    private float range = 100f;
+    
+
+	// Use this for initialization
+	void Start () {
+        // Ray cast initialization
+        laserLine = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Ray cast finds ray casted object
+        RaycastHit hit;
+        Ray laserRay = new Ray(laserBegin.position, laserBegin.transform.forward);
+        laserLine.SetPosition(0, laserBegin.position);
+        Vector3 rayDirection = laserEnd.position - laserBegin.position;
+        if (Physics.Raycast(laserRay, out hit, 100)){
+            laserLine.SetPosition(1, hit.point);
+            hitpoint = hit.point;
+            selectedItem = hit.collider.gameObject;
+        }
+        else {
+            laserLine.SetPosition(1, laserBegin.position + (rayDirection * range));
+            selectedItem = null;
+        }
+        // Trigger clicked -> raycast to handle chosen object
+        if (!clicked && OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controller) > triggerThreshold) {
+            if(selectedItem != null){
+                menu.GetComponent<Achievements>().clickHandle(selectedItem);
+                clicked = true;
+            }
+        } 
+        // Trigger released -> allow for another click
+        if (clicked && OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controller) < triggerThresholdLower) {
+            clicked = false;
+        }
+	}
+}
