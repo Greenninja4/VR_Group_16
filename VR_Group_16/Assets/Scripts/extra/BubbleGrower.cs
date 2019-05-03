c6ecc98c342f8df3112690934aaf482ee2082a8f
Greenninja4 <johnmborn4@gmail.com>
2019-05-03 08:32:30 -0500

    Lava scene

---
+++ VR_Group_16/Assets/Scripts/extra/BubbleGrower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleGrower : MonoBehaviour {
+
+	private float growFactor = 0.3f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float time_diff = Time.deltaTime;
+		transform.localScale = (1 + time_diff * growFactor) * transform.localScale;
+	}
+}
